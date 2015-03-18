b87b2e154829896cfb6d750247b73b24124413cb
AdamStone <arstone208@gmail.com>
2015-03-18 20:59:12 +0100

    callback after setup-db

--- backend/setup-db.js
+++ backend/setup-db.js
@@ -3,7 +3,7 @@
 var db = require('./config/database.js');
 
 module.exports = {
-  initialize: function() {
+  initialize: function(callback) {
     var queries = [
       'CREATE CONSTRAINT ON (user:User) ASSERT user.id IS UNIQUE',
       'CREATE CONSTRAINT ON (user:User) ASSERT user.email IS UNIQUE',
@@ -14,19 +14,25 @@ module.exports = {
 //      'CREATE CONSTRAINT ON (gametype:Gametype) ASSERT gametype.name IS UNIQUE'
     ];
 
+    var finished = 0;
+
     var timeout = function(cypher) {
       db.queryFactory(cypher, {}, function(err, result) {
         if (err) {
           return console.error(err);
         }
         if (result) {
+          finished++;
           console.log(result);
+          if (finished === queries.length) {
+            return callback();
+          }
         }
       }).send();
     };
-      
+
     for (var i=0; i < queries.length; i++) {
-      setTimeout(timeout, 500*(i+1), queries[i]);
+      setTimeout(timeout, 100*(i+1), queries[i]);
     }
   }
 };
